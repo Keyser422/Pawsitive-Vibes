d4caccf66eb394b03fc9132653f1b2524b59cfcd
Grace Lee <gracehlee.070@gmail.com>
2024-04-15 22:36:13 -0700

    feat: SQL table users and pets

--- data/dogs.sql
+++ data/dogs.sql
@@ -1,4 +1,17 @@
+CREATE TABLE users (
+    id SERIAL NOT NULL UNIQUE,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    phone_number TEXT NOT NULL,
+    bio TEXT DEFAULT NULL,
+    date_created DATETIME NOT NULL
+);
+
 CREATE TABLE pets (
     id SERIAL NOT NULL UNIQUE,
-    
+    dog_name TEXT NOT NULL,
+    image_url TEXT NOT NULL,
+    for_sale BOOLEAN NOT NULL DEFAULT FALSE,
+    price INTEGER DEFAULT NULL,
 );
