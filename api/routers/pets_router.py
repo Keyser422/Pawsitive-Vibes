49271ad4f8eea40ea523be87a08be10c94dc3a3d
Grace Lee <gracehlee.070@gmail.com>
2024-04-16 22:33:05 -0700

    feat: create pets_routers.py

---
+++ api/routers/pets_router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends
+from typing import List, Optional
+from models.pets import PetIn, PetInUpdate, PetOut
+from queries.pet_queries import PetQueries
+
+router = APIRouter()
+
+
+@router.get("/api/pets", response_model=List(PetOut))
+def get_all_pets(
+    repo: PetQueries = Depends(),
+) -> PetOut:
+    return repo.get_all()
