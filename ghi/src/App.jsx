17da051c3e0ca253eba669b312e590696f079b8a
Grace <gracehlee.070@gmail.com>
2024-04-22 15:48:00 -0700

    feat: create Nav.js file, import and use it in App.jsx file

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Nav from './app/Nav'
 
 function App() {
     return (
         <BrowserRouter>
             <div className="container">
+                <Nav />
                 <Routes>
                     <div>test</div>
                 </Routes>
             </div>
         </BrowserRouter>
-    );
+    )
 }
 
 export default App
