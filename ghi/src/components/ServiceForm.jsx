9fccf24c84b31669554f53c408fee7ce476b8d78
Austin Hamilton <ahamiltonnn@outlook.com>
2024-04-23 14:21:08 -0700

    initial commit

---
+++ ghi/src/components/ServiceForm.jsx
@@ -0,0 +1,20 @@
+import React, { useSate, useState } from 'react';
+
+function ServiceForm () {
+    const [service, setService] = useSate('');
+    const [pictureUrl, setPictureUrl] = useSate('');
+    const [duration, setDuration] = useState('');
+    const [cost, setCost] = useSate('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const data = {};
+        data.service = service;
+        data.picture_url = pictureUrl;
+        data.duration = duration;
+        data.cost = cost;
+
+        const url
+    }
+}
