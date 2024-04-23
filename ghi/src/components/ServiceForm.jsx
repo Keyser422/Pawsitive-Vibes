798dc3be892e88a00ffd573afc1cec5391d47a2d
Austin Hamilton <ahamiltonnn@outlook.com>
2024-04-23 15:39:45 -0700

    still working on form

--- ghi/src/components/ServiceForm.jsx
+++ ghi/src/components/ServiceForm.jsx
@@ -16,6 +16,68 @@ function ServiceForm () {
         data.cost = cost;
 
         const url = "http://localhost:8000/api/services/";
-        
+        const fetchConfig = {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            },
+        };
+
+        const response = await fetch(url, fetchConfig);
+        if (response.ok) {
+            const newService = await response.json();
+            setService('');
+            setPictureUrl('');
+            setDuration('');
+            setCost('');
+        }
     }
+
+
+    const handleServiceChange = (e) => {
+        const value = e.target.value;
+        setService(value)
+    }
+
+    const handlePictureUrlChange = (e) => {
+        const value = e.target.value
+        setPictureUrl(value)
+    }
+
+    const handleDurationChange = (e) => {
+        const value = e.target.value
+        setDuration(value)
+    }
+
+    const handleCostChange = (e) => {
+        const value = e.target.value
+        setCost(value)
+    }
+
+
+    return (
+        <div className="row">
+            <div className="offset-3 col-6">
+                <div className="shadow p-4 mt-4">
+                    <h1>Create a Service</h1>
+                    <form onSubmit={handleSubmit} id="create-service-form">
+                        <div className="form-floating mb-3">
+                            <input
+                                value={service}
+                                onChange={handleServiceChange}
+                                placeholder="Service Name"
+                                required
+                                type="text"
+                                name="service"
+                                id="service"
+                                className="form-control"
+                            ></input>
+                            <label htmlFor="service">Service</label>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
