8c4a8330aad4451d0aceb6f1160b6803f01e6c3f
Grace <gracehlee.070@gmail.com>
2024-04-23 11:39:43 -0700

    fix: remove router error

--- ghi/src/main.jsx
+++ ghi/src/main.jsx
@@ -2,9 +2,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import SignInForm from './components/SignInForm'
 import SignUpForm from './components/SignUpForm'
-import App from './App.jsx'
+import App from './App'
 import AuthProvider from './components/AuthProvider'
 
 import './index.css'
