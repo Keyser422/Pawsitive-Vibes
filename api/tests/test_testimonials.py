7f5984bf6ad8851b7cc411073455244bfc15a83d
James <jamesrkeyser22@gmail.com>
2024-05-07 18:03:41 -0600

    unit test

---
+++ api/tests/test_testimonials.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+from main import app
+from queries.testimonials_queries import TestimonialRepository
+
+client = TestClient(app)
+
+
+class EmptyTestimonialQueries:
+    def get_all(self):
+        return []
+
+
+def test_get_all_services():
+    # ARRAGNE
+    app.dependency_overrides[TestimonialRepository] = EmptyTestimonialQueries
+
+    # ACT
+    response = client.get("/api/testimonials")
+    app.dependency_overrides = {}
+
+    # ASSERT
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Testimonials not found.'}
